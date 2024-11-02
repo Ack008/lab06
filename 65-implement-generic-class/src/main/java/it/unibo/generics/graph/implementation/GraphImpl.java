eaaa534742fd036fba4bdbded83f12652971a5ea
Francesco Sacripantte <francesco.sacripante@tudio.unibo.it>
2024-11-02 01:12:54 +0100

    Implementing addNode, addEdge and constructor of graphImpl

--- 65-implement-generic-class/src/main/java/it/unibo/generics/graph/implementation/GraphImpl.java
+++ 65-implement-generic-class/src/main/java/it/unibo/generics/graph/implementation/GraphImpl.java
@@ -1,47 +1,60 @@
 package it.unibo.generics.graph.implementation;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import it.unibo.generics.graph.api.Graph;
 
-public class GraphImpl implements Graph{
+public class GraphImpl <N> implements Graph<N>{
+    private final Map<N, Set<N>> adjacencyMap;
 
-    private final Map<String, Set<String>> adjaciencyList;
     GraphImpl(){
-        adjaciencyList = new HashMap<>();
+        adjacencyMap = new HashMap<>();
     }
-    
+
     @Override
-    public void addEdge(Object source, Object target) {
-        // TODO Auto-generated method stub
+    public void addEdge(N source, N target) {
+        if(source != null && target != null){
+            if(!adjacencyMap.containsKey(target)){
+                throw new IllegalArgumentException("target node wasn't added");
+            }
+            if(!adjacencyMap.containsKey(source)){
+                throw new IllegalArgumentException("source node wasn't added");
+            }
+            adjacencyMap.get(source).add(target);
+        }
+    }
+
+    @Override
+    public void addNode(N node) {
+        if(node == null){
+            throw new IllegalArgumentException("L'argomento è nullo");
+        }
+        if(!adjacencyMap.containsKey(node)){
+            adjacencyMap.put(node, new HashSet<>());
+        }
         
     }
 
     @Override
-    public void addNode(Object node) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public List getPath(Object source, Object target) {
+    public List<N> getPath(N source, N target) {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public Set linkedNodes(Object node) {
+    public Set<N> linkedNodes(N node) {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public Set nodeSet() {
+    public Set<N> nodeSet() {
         // TODO Auto-generated method stub
         return null;
     }
-    
+
 }
