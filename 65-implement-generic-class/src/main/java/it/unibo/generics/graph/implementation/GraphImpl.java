c067c831beece7401d99f28e9a378297ea512645
Francesco Sacripantte <francesco.sacripante@tudio.unibo.it>
2024-11-02 02:37:01 +0100

    Implementing the bfs

--- 65-implement-generic-class/src/main/java/it/unibo/generics/graph/implementation/GraphImpl.java
+++ 65-implement-generic-class/src/main/java/it/unibo/generics/graph/implementation/GraphImpl.java
@@ -1,7 +1,9 @@
 package it.unibo.generics.graph.implementation;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -15,7 +17,7 @@ public class GraphImpl <N> implements Graph<N>{
     GraphImpl(){
         this(TypeOfPathSearch.DEPTH_FIRST);
     }
-    
+
     GraphImpl(TypeOfPathSearch type){
         typeOfPath = type;
         adjacencyMap = new HashMap<>();
@@ -46,10 +48,42 @@ public class GraphImpl <N> implements Graph<N>{
         
     }
 
+    List<N> bfs(N source, N target){
+        if(source.equals(target)){
+            return List.of(source);
+        }
+        List<N> stack = new LinkedList<>();
+        Set<N> visited = new HashSet<>();
+        Map<N,N> fatherSonMap = new HashMap<>();
+        visited.add(source);
+        stack.addFirst(source);
+        fatherSonMap.put(source, null);
+        N nextNode = source;
+        while(!stack.isEmpty() && !nextNode.equals(target)){
+            nextNode = stack.removeFirst();
+            for(var adjacentNode : adjacencyMap.get(nextNode)){
+                if(!visited.contains(adjacentNode)){
+                    fatherSonMap.put(adjacentNode, nextNode);
+                    stack.addFirst(adjacentNode);
+                }
+            }
+            visited.add(nextNode);
+        }
+        List<N> path = new LinkedList<>();
+        N node = target;
+        while(node != null){
+            path.addFirst(node);
+            node = fatherSonMap.get(node);
+        }
+        return path;
+    }
+
     @Override
     public List<N> getPath(N source, N target) {
-        // TODO Auto-generated method stub
-        return null;
+        if(source == null || target == null){
+            throw new IllegalArgumentException();
+        }
+        return bfs(source, target);
     }
 
     @Override
